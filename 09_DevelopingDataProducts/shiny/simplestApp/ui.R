7a501d6097792e87f906190f2a874cb4e16acf7e
Brian Caffo <bcaffo@gmail.com>
2014-01-15 15:22:41 -0500

    finished first shiny lecture

--- 09_DevelopingDataProducts/shiny/simplestApp/ui.R
+++ 09_DevelopingDataProducts/shiny/simplestApp/ui.R
@@ -2,7 +2,8 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Hello Shiny!"),
   sidebarPanel(
-    h3('Sidebar text')
+      h1('H1 text')
+      h2('h2 Text')
   ),
   mainPanel(
       h3('Main Panel text')
